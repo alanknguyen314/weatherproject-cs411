751bf5fdb4735a71b196603fae07d3ea503b5e85
Alan K Nguyen <alanknguyen314@gmail.com>
2023-06-28 07:39:13 -0400

    Add OAuthentication via Google OAuth
    Add OAuthentication via Google OAuth. changes; urls.py, settings.py, add a test file called index1.html

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import register_request, login_request
 from .views import logout_request, index, profile
+
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -11,5 +14,8 @@ urlpatterns = [
     path("logout", logout_request, name="logout"),
     path("weather", index, name="weather"),
     path("profile", profile, name="profile"),
+    path('accounts/', include('allauth.urls')),
+    path('logout-oauth', LogoutView.as_view()),
+    path('index', TemplateView.as_view(template_name="index.html"))
     # your other paths...
 ]
--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'weatherapp', # new
-    'crispy_forms'
+    'crispy_forms',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 
@@ -126,3 +130,25 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = './templates/weatherapp/profile.html'
+LOGOUT_REDIRECT_URL = './templates/weatherapp/weather.html'
