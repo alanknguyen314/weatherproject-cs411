f5ee113a6419e65eb633e54675ea598fb770bbe6
Alan K Nguyen <alanknguyen314@gmail.com>
2023-06-28 06:33:18 -0400

    Update models.py

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class VisitedList(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+
+  def get_absolute_url(self):
+      return reverse("visited-list-detail", kwargs={"pk": self.pk})
+
+  def __str__(self):
+    return self.title
